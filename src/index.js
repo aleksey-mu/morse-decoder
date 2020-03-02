d898ca926c194e8d45a071ce444a99a28d0659c5
aleksey_mu <pacuklex@mail.ru>
2020-03-03 02:11:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,45 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    const expression = expr.split('');
+    const exprArr = [];
+    for (let i = 0; i < expression.length; i += 10) {
+        exprArr.push(expression.slice(i, i + 10).join(''));
+    }
+
+    const clearArr = [];
+    for (let char of exprArr) {
+        const numArr = char.split('');
+        for (let i = 0; i < 10; i += 1) {
+            if (numArr[i] !== '0') {
+                clearArr.push(numArr.slice(i).join(''));
+                break;
+            }
+        }
+    }
+
+    const morseArr =[];
+    for (let char of clearArr) {
+        const morseCharArr = [];
+        for (let i = 0; i < char.length; i += 2) {
+            let morseSymbol = (char.split('').slice(i, i + 2).join(''));
+            if (morseSymbol ===  '10') {
+                morseCharArr.push('.');
+            } else if (morseSymbol ===  '11') {
+                morseCharArr.push('-');
+            } else if (morseSymbol ===  '**') {
+                morseCharArr.push(' ');
+            }
+            
+        }
+        morseArr.push(MORSE_TABLE[morseCharArr.join('')]);
+    }
+   
+    return morseArr.join('');
 }
 
 module.exports = {
